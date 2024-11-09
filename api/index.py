5d0ea31e040b0853b081f1637636a39d84d5f573
Mukeshwaran Baskaran <mukesh96official@gmail.com>
2024-11-08 22:10:03 -0600

    modall

--- api/index.py
+++ api/index.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_folder='../static',
+            template_folder='../templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
